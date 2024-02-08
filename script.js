7a719e3d4a2ff533137b02f031ba84b24629a6c2
helena <helena@helenas-MacBook-Pro.local>
2024-02-08 11:18:19 -0800

    Push

---
+++ script.js
@@ -0,0 +1,7 @@
+"use strict";
+
+let firstString = "this is our first string. yay!";
+let firstNumber = 42;
+
+// connect to html elements
+const strElement = document.querySelector("#first-string");
